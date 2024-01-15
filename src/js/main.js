3a4036c8506397da8b46700e68cdd10a28839285
RafaGui <rafaelwilliansgi4@gmail.com>
2024-01-15 17:58:56 -0300

    atualization

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+let imgs = document.getElementById("img");
+let img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+    if(idx > img.length -1){
+        idx =0;
+    }
+    imgs.style.transform = `translateX(${-idx * 300}px)`;
+}
+
+setInterval(carrossel, 5000);
